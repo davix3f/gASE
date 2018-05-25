3a828fc00c8bcf29e2795cf0675ba693faa5e9d2
Davide <davide_fiorito@libero.it>
2018-05-25 19:17:03 +0200

    Half working

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import gi
 import re
+
+import line_analysis
+line_parse = line_analysis.line_parse
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
@@ -12,33 +16,14 @@ with open("/etc/apt/sources.list", "r") as srcs_file:
         if line is not "" or "\n":
             lines.append(line)
 
-
-def get_reponame(line):
-    try:
-        commented = re.search(r"$#", line)
-        http_s = re.search(r"(http|https)://", line)
-        domain = re.search(r"(?<=://.+)(\s)", line)
-        release_branch = re.search(r")
-        free, 
-        contrib, 
-        free
-
-    except:
-        return(False)
-    if free is "":
-        free_state = True
+for item in lines:
+    if line_parse(item) != False:
+        datas.append(line_parse(item))
+        print(line_parse(item))
     else:
-        free_state = False
-    return(commented, http_s, domain, release_branch, free, contrib, free_state)
-    #
+        print("[-no match for]", item.rstrip())
 
 
-for key, value in enumerate(lines):
-    if get_reponame(value) is not False:
-        print("[-#-match-#-]", value)
-        datas.add(get_reponame(value))
-    else:
-        print("[-!-line got false-!-]", value)
 
 class CRenderTxt(Gtk.Window):
 
@@ -47,27 +32,47 @@ class CRenderTxt(Gtk.Window):
 
         self.set_default_size(400, 400)
 
-        self.liststore = Gtk.ListStore(str, str)
+        self.liststore = Gtk.ListStore(bool, bool, bool, str, str, bool, bool, bool, bool)
         for item in datas:
-            self.liststore([ ])
+            self.liststore.append([*item])
 
 
         treeview = Gtk.TreeView(model=self.liststore)
-        renderer_text = Gtk.CellRendererText()
-        column_http_s = Gtk.TreeViewColumn("HyTxProtocol", renderer_text, text=0)
-        treeview.append_column(column_http_s)  #
 
-        renderer_editabletext = Gtk.CellRendererText()
+        renderer_text = Gtk.CellRendererText()  # render text
+
+        renderer_editabletext = Gtk.CellRendererText()  # editable text renderer
         renderer_editabletext.set_property("editable", True)
 
-        column_domain = Gtk.TreeViewColumn("Domain", renderer_editabletext, text=1)
-        treeview.append_column(column_domain)  #
+        renderer_toggle = Gtk.CellRendererToggle()
+        renderer_toggle.connect("toggled", self.on_cell_toggled)
 
-        column_rbranch = Gtk.TreeViewColumn("Branch", renderer_editabletext, text=1)
-        treeview.append_column(column_rbranch)  #
 
-        column_free = Gtk.TreeViewColumn("Free-ness", renderer_text, text=0)
-        treeview.append_column(column_free) #
+        column_comment = Gtk.TreeViewColumn("Commented", renderer_toggle, active=1)
+        treeview.append_column(column_comment)
+
+        column_binary = Gtk.TreeViewColumn("Binary", renderer_toggle, active=1)
+        treeview.append_column(column_binary)
+
+        column_https = Gtk.TreeViewColumn("HTTP", renderer_toggle, active=1)
+        treeview.append_column(column_https)
+
+        column_domain = Gtk.TreeViewColumn("URL", renderer_editabletext, text=1)
+        treeview.append_column(column_domain)
+
+        column_branch = Gtk.TreeViewColumn("Branch", renderer_editabletext, text=1)
+        treeview.append_column(column_branch)
+
+        column_main = Gtk.TreeViewColumn("Main", renderer_toggle, active=1)
+        treeview.append_column(column_main)
+
+        column_contrib = Gtk.TreeViewColumn("Contrib", renderer_toggle, active=1)
+        treeview.append_column(column_contrib)
+
+        column_free = Gtk.TreeViewColumn("Free", renderer_toggle, active=1)
+        treeview.append_column(column_free)
+
+        column_ftp = Gtk. TreeViewColumn("FTP", renderer_toggle, active=0)
 
         renderer_editabletext.connect("edited", self.text_edited)
 
@@ -76,11 +81,11 @@ class CRenderTxt(Gtk.Window):
     def text_edited(self, widget, path, text):
         self.liststore[path][1] = text
 
+    def on_cell_toggled(self, widget, path):
+        self.liststore[path][1] = not self.liststore[path][1]
+
 
 win = CRenderTxt()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
-
-
-
