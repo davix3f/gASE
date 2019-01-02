1dde74c15065e5acacfe55c1b19881c450b4e684
Davide <davide_fiorito@libero.it>
2019-01-02 01:35:41 +0100

    New branch detector
    With support extended to multiple branches

--- src/gASEline_parse.py
+++ src/gASEline_parse.py
@@ -4,15 +4,17 @@ import re
 basic_elements = r"(?P<comment>^#(\s*))?"\
               "(?P<binary_src>(deb|deb-src){1})(\s+)"\
               "(?P<URL>((http://|https://)?(\S*)))"\
-              "\s+"\
-              "(?P<branch>(\S+))"\
 
 
 def line_parse(line):
+
+    strippable_line = line
+    branch = ""
+
     if type(line) is not str:
         raise TypeError("argument not <str>")
     try:
-        comment, binary_src, URL, branch = re.search(basic_elements, line).group("comment", "binary_src", "URL", "branch")
+        comment, binary_src, URL = re.search(basic_elements, line).group("comment", "binary_src", "URL")
     except:
         return False
 
@@ -20,29 +22,42 @@ def line_parse(line):
         commented = False
     else:
         commented = True
+        strippable_line = strippable_line.replace("#", "")
+
     if re.search(r"non-free", line):  # free T/F
         free = False
+        strippable_line = strippable_line.replace("non-free", "")
     else:
         free = True
+
     if re.search(r"(\s+)main(\s+)", line):  # main T/F
         main = True
+        strippable_line = strippable_line.replace("main", "")
     else:
         main = False
+
     if re.search(r"contrib", line):  # contrib T/F
         contrib = True
+        strippable_line = strippable_line.replace("contrib", "")
     else:
         contrib = False
-    if re.search(r"ftp", line):  # ftp T/F
-        ftp = True
-    else:
-        ftp = False
+
     if binary_src == "deb-src":  # binary T/F
         binary = False
+
     if binary_src == "deb":
         binary = True
+    
+    strippable_line = strippable_line.replace(str(URL), "")
+    strippable_line = strippable_line.replace(str(binary_src), "")
 
     if re.search(r"cdrom:", line):
         #URL = re.search(r"(?<=(cdrom:)).*]/?", line).group()
         URL = re.search(r"cdrom:.*]/?", line).group()
 
-    return(commented, binary, str(URL), str(branch), main, contrib, free, ftp, line.rstrip())
+    strippable_line = list(filter(lambda w: w != '', strippable_line.split(" ")))
+    
+    for item in strippable_line:
+        branch += item + " " if strippable_line.index(item) <= len(strippable_line)  else ""
+
+    return(commented, binary, str(URL), str(branch), main, contrib, free, line.rstrip())
